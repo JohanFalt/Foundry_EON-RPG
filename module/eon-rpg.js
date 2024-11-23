e528195f9ad9b02138cc787a5bdd8b654500c3a1
Johan Fält <57871596+JohanFalt@users.noreply.github.com>
2024-11-23 18:27:21 +0100

    ups

--- module/eon-rpg.js
+++ module/eon-rpg.js
@@ -73,7 +73,7 @@ Hooks.once("ready", async () => {
     // Do anything once the system is ready
 	const installedVersion = game.settings.get("eon-rpg", "systemVersion");
     const systemVersion = game.data.system.version;
-    const isDemo = true;
+    const isDemo = false;
 
     if (game.user.isGM) {
         if (((installedVersion !== systemVersion || installedVersion === null)) || (isDemo)) {
