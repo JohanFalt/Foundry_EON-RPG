2bc0de0f7c94834acdf5cbdca03e8325fcc01521
Johan Fält <57871596+JohanFalt@users.noreply.github.com>
2026-03-26 07:46:29 +0100

    v5.1.2

--- module/migration.js
+++ module/migration.js
@@ -423,15 +423,23 @@ export async function DoNotice(systemVersion, installedVersion, isDemo = false) 
         <h4>Eon stridsmodul</h4>  
         <p>Denna modulen hanterar turordningen i strid. Den är för närvarande i beta och kan användas som ett alternativ till Foundrys inbyggda stridsmodul.</p>
         `;
-    
-    
-    if (await CompareVersion(installedVersion, '5.1.1', isDemo)) {
+    }
+
+    if (await CompareVersion(installedVersion, '5.1.2', isDemo)) {
+         partMessage += `
+         <p>
+             <p>Visualiserar nu folkslagsegenskaper i Bakgrundsfliken i Eon 5.</p>
+         </p>           
+         `;
+    }
+	
+	if (await CompareVersion(installedVersion, '5.1.1', isDemo)) {
          partMessage += `
          <p>
              <p>Ett fel hittades där man inte kunde hantera rustningar korrekt.</p>
          </p>           
          `;
-    }}
+    }
 
     if (await CompareVersion(installedVersion, '5.1.0', isDemo)) {
          partMessage += `
